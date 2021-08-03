65f7a0ba759d85a57edba9bf6cad1dd8e3688454
jamesliao <JamesCoding888@hotmail.com>
2021-08-04 00:53:27 +0800

    java

--- spring/src/main/java/com/study/spring/case01/Person.java
+++ spring/src/main/java/com/study/spring/case01/Person.java
@@ -2,18 +2,22 @@ package com.study.spring.case01;
 
 public class Person {
 	private Integer id;
-	private String nameString;
+	private String name;
 	public Integer getId() {
 		return id;
 	}
 	public void setId(Integer id) {
 		this.id = id;
 	}
-	public String getNameString() {
-		return nameString;
+	public String getName() {
+		return name;
 	}
-	public void setNameString(String nameString) {
-		this.nameString = nameString;
+	public void setName(String name) {
+		this.name = name;
+	}
+	@Override
+	public String toString() {
+		return "Person [id=" + id + ", name=" + name + "]";
 	}
 	
 }
