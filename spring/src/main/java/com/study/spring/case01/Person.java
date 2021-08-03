4eb20851603707d3e9cba9b232e833bd65de0ba9
jamesliao <JamesCoding888@hotmail.com>
2021-08-04 00:36:46 +0800

    java

---
+++ spring/src/main/java/com/study/spring/case01/Person.java
@@ -0,0 +1,19 @@
+package com.study.spring.case01;
+
+public class Person {
+	private Integer id;
+	private String nameString;
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getNameString() {
+		return nameString;
+	}
+	public void setNameString(String nameString) {
+		this.nameString = nameString;
+	}
+	
+}
---
+++ spring/src/main/java/com/study/spring/case01/Test.java
@@ -0,0 +1,14 @@
+package com.study.spring.case01;
+
+public class Test {
+
+	public static void main(String[] args) {
+		Person person = new Person();
+		
+		person.setId(1);
+		person.setNameString("Spring");
+		
+		System.out.println(person);
+	}
+
+}
